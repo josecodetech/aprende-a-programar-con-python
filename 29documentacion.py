44efcb62e3420033cfef11a159a6ca9b3a8be1b5
Jose Ojeda <josecodetech@gmail.com>
2023-01-01 11:11:45 +0100

    ayuda

---
+++ 29documentacion.py
@@ -0,0 +1,13 @@
+def sumar(num1, num2):
+    """_summary_
+        Suma dos numeros
+        num1 int
+        num2 int
+        return num1 + num2
+
+    """
+    return num1+num2
+
+
+if __name__ == '__main__':
+    print(sumar(25, 30))
